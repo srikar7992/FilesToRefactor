8c3acfae48e38072f840ab2d3409b75a2bf05844
srikar7992 <101961313+srikar7992@users.noreply.github.com>
2023-04-04 22:18:10 +0530

    Add files via upload

---
+++ UpfrontCosts_Partial.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RentaMetrixUI.Pages.Portfolio
+{
+    public class UpfrontCosts_PartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
